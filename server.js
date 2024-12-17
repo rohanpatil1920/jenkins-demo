cc1e5a8bc74eaca294cfbb6abe0f37efe137d828
Rohan Patil <rohanpatil762.rp@gmail.com>
2024-12-17 22:17:58 +0530

    fourth Commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.get("/",(req,res)=>{
-    res.send("Hi this server is running on port 4000, app v0.2")
+    res.send("Hi this server is running on port 4000, app v1.0\n My App is running through Jenkins CI/CD Pipeline")
 })
 
 app.listen(4000,'0.0.0.0',()=>{
