83199550c2110093441b49cb50d4d79b5bcfa11a
Rohan Patil <rohanpatil762.rp@gmail.com>
2024-12-17 19:19:25 +0530

    second commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.get("/",(req,res)=>{
-    res.send("Hi this server is running on port 4000")
+    res.send("Hi this server is running on port 4000:4000, app v0.1")
 })
 
 app.listen(4000,'0.0.0.0',()=>{
